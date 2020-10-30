434b5c2aa5e78078ea8215f43fa6e3b946c61704
Josephshitandi <jsphshtnd@gmail.com>
2020-10-30 14:39:45 +0300

    Add logout method that enables user to logout of the application

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,3 +36,9 @@ def register():
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
